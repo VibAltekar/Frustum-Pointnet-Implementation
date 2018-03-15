984c472bcba15bcc8d75ebb864f99e63e0f31bc1
VibAltekar <altekar.vibhav@gmail.com>
2018-03-15 01:31:38 -0700

    automatic gitpush

---
+++ feature_pyramid_net.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+class tent(nn.Module):
+    stride_val = 4
+    def __init__(self, in_planes, planes, stride=1):
+        super(tent, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.stride_val*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.stride_val*planes)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.stride_val*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.stride_val*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.stride_val*planes))
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class FPN(nn.Module):
+    def __init__(self, block, num_blocks):
+        super(FPN, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self.make_layer(block,  64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)  # Reduce channels
+        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d( 512, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer3 = nn.Conv2d( 256, 256, kernel_size=1, stride=1, padding=0)
+
+    def make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.stride_val
+        return nn.Sequential(*layers)
+
+    def upsample_add(self, x, y):
+        _,_,H,W = y.size()
+        return F.upsample(x, size=(H,W), mode='bilinear') + y
+
+    def forward(self, x):
+        c1 = F.relu(self.bn1(self.conv1(x)))
+        c1 = F.max_pool2d(c1, kernel_size=3, stride=2, padding=1)
+        c2 = self.layer1(c1)
+        c3 = self.layer2(c2)
+        c4 = self.layer3(c3)
+        c5 = self.layer4(c4)
+        p5 = self.toplayer(c5)
+        p4 = self.upsample_add(p5, self.latlayer1(c4))
+        p3 = self.upsample_add(p4, self.latlayer2(c3))
+        p2 = self.upsample_add(p3, self.latlayer3(c2))
+        p4 = self.smooth1(p4)
+        p3 = self.smooth2(p3)
+        p2 = self.smooth3(p2)
+        return p2, p3, p4, p5
+
+
+def FPN101():
+    return FPN(tent, [2,2,2,2])
+def test():
+    net = FPN101()
+    fms = net(Variable(torch.randn(1,3,600,900)))
+    for fm in fms:
+        print(fm.size())
+
+test()
